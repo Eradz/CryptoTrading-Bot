56e649d18930139aa66c83935a1f6e6f926ca0db
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2025-10-31 14:02:12 +0100

    Refrac: Trading route refractored

--- controllers/Trade/tradeController.js
+++ controllers/Trade/tradeController.js
@@ -1,6 +1,7 @@
 import AsyncHandler from "express-async-handler";
 import { AppResponse } from "../../utils/index.js";
 import { AuthenticateExchange } from "../../utils/AuthenticateExchange.js";
+import { trade } from "../../utils/trade/trade.js";
 
 export const createTradeOrder = AsyncHandler(async (req, res) => {
     const { symbol, side, type, amount, price } = req.body;
@@ -10,6 +11,6 @@ export const createTradeOrder = AsyncHandler(async (req, res) => {
         return AppResponse.error(res, "User ID and Exchange ID are required");
     }
     const authenticatedExchange = await AuthenticateExchange({ userId, exchangeId });
-    const order = await authenticatedExchange.createOrder(symbol, side, type, amount, price);
+    const order = await trade(symbol, side, amount, authenticatedExchange, type, price);
     AppResponse.success(res, "Order created successfully", order);
 });
